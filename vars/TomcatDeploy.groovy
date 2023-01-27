1f19ce65a02aca73d46fdc5f15df772f7888f96c
yeswanth1434 <119117435+yeswanth1434@users.noreply.github.com>
2023-01-27 23:14:27 +0530

    Update TomcatDeploy.groovy

--- vars/TomcatDeploy.groovy
+++ vars/TomcatDeploy.groovy
@@ -1,6 +1,6 @@
 def call (ip,creds,user) {
    sshagent(["${creds}"]) {
-       sh "scp -o StrictHostKeyChecking=no target/*hello.war ${user}@${ip}:/opt/tomcat9/webapps"
+       sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps"
        sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
        sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
    }
